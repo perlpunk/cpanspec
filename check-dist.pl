1120676e70798947c2426bbc2cce9fa144211b09
Tina Müller <tina.mueller@suse.com>
2023-07-23 23:25:37 +0200

    WIP

--- check-dist.pl
+++ check-dist.pl
@@ -20,11 +20,25 @@ $rows = read_tsv($modules);
 my %cpandist;
 for my $row (@$rows) {
     my ($dist, $distv, $module, $version) = @$row;
+    $distv =~ s/^v//;
     $cpandist{ $dist } = [$distv, $module, $version];
 }
 for my $name (sort keys %dlpdist) {
     unless (exists $cpandist{ $name }) {
         warn __PACKAGE__.':'.__LINE__.$".Data::Dumper->Dump([\$name], ['name']);
+        next;
+    }
+    my $cpan = $cpandist{ $name };
+    my $dlp = $dlpdist{ $name };
+    unless (defined $dlp) {
+        warn __PACKAGE__.':'.__LINE__.": ============== $name\n";
+        warn __PACKAGE__.':'.__LINE__.$".Data::Dumper->Dump([\$name], ['name']);
+        next;
+    }
+    if ($dlp ne $cpan->[0]) {
+        warn __PACKAGE__.':'.__LINE__.": ============== $name\n";
+        warn __PACKAGE__.':'.__LINE__.$".Data::Dumper->Dump([\$cpan], ['cpan']);
+        warn __PACKAGE__.':'.__LINE__.$".Data::Dumper->Dump([\$dlp], ['dlp']);
     }
 }
 
